986b55c60fe364ff36c10dbe3bf224d930755a52
Hobbytowo <weronika.ak@wp.pl>
2018-08-02 22:01:08 +0200

    separate getData() and getColorsArray() from createPieChart(), fix bug when percent sum !== 100%

--- src/js/index.js
+++ src/js/index.js
@@ -18,13 +18,13 @@ const createDataForm = nr => {
 
       divSlice.innerHTML = `
         <h3 class="title">Slice ${ i + 1 }</h3>
-        <divclass="form__data">
+        <div="form__data">
           <label for="inputName${ i }" class="form__label">Name: </label>
-          <input id="inputName${ i }" type="text" class="form__input sliceName"></input>
+          <input id="inputName${ i }" type="text" class="form__input sliceName">
         </div>
         <div class="form__data data data--value">
           <label for="inputValue${ i }" class="form__label">Value: </label>
-          <input id="inputValue${ i }" type="number" class="form__input sliceValue" placeholder="Write only numbers"></input>
+          <input id="inputValue${ i }" type="number" class="form__input sliceValue" placeholder="Write only numbers">
         </div>
       `
 
@@ -33,11 +33,11 @@ const createDataForm = nr => {
   }
 
   if (currentNrOfSlices <= nr && chartContainer.style.display === "flex") {
-    drawPieChart()
+    createPieChart()
   }
 }
 
-// event for sumbit first form
+// event for sumbit first form and create second form
 
 const buttonMainData = document.querySelector('.button--mainData')
 
@@ -59,75 +59,82 @@ const getValues = inputsClass => {
   return dataNamesArr
 }
 
-// creating chart
+// generating table of colors for slices
 
-const drawPieChart = () => {
-  const chartContainer = document.querySelector('.article--chart')
-  chartContainer.style.display = "flex"
+const hexToDec = val => parseInt(val, 16)
+const decToHex = val => val !== 0 ? val.toString(16) : '00'
 
-  const chartTitle = document.querySelector('#chartTitle').value
-  const titleContainer = document.querySelector('.title--chart')
-  titleContainer.textContent = chartTitle
+const getColorsArray = (col1, nrOfSlices) => {
+  const getHexaColorsArray = rgb => {
+    const step = ~~((255 - Math.min(...rgb)) / nrOfSlices)
+    const getNewColor = (rgb, i) => rgb.map(x => Math.min(x + step * i, 255))
+    const getNewRGB = (col, nr) => Array(nr).fill(col).map(getNewColor)
+    const newRGB = getNewRGB(rgb, nrOfSlices)
+    return newRGB.map(rgb => ['#', ...rgb.map(decToHex)].join(''))
+  }
+  return getHexaColorsArray(col1)
+}
 
+// getting neessery data for draw chart
+
+const getData = () => {
   const dataNames = getValues('sliceName')
   const dataValues = getValues('sliceValue')
-  const numberOfValues = dataValues.length
+  const numberOfSlices = dataValues.length
+
   const leadingColor = document.querySelector('.input--color').value
+  const leadingColorRGB = leadingColor.match(/../g).map(hexToDec)
+  const colors = getColorsArray(leadingColorRGB, numberOfSlices)
 
-  // Colors functions generating table of colors for slices
-
-  const getColorsTable = () => {
-    const hexToDec = val => parseInt(val, 16)
-    const decToHex = val => val !== 0 ? val.toString(16) : '00'
-    const leadingColorRGB = leadingColor.match(/../g).map(hexToDec)
-
-    const getHexaColorsTable = rgb => {
-      const step = ~~((255 - Math.min(...rgb)) / numberOfValues)
-      const getNewColor = (rgb, i) => rgb.map(x => Math.min(x + step * i, 255))
-      const getNewRGB = (leadingColor, nrOfColors) => Array(nrOfColors).fill(leadingColor).map(getNewColor)
-      const newRGB = getNewRGB(rgb, numberOfValues)
-      return newRGB.map(rgb => ['#', ...rgb.map(decToHex)].join(''))
-    }
-
-    return getHexaColorsTable(leadingColorRGB)
-  }
-
-  const colors = getColorsTable()
-
-  // creating data array
+  // creating data array from the biggest to the smallest
 
   const sum = dataValues.reduce((a, b) => a * 1 + b * 1, 0)
   const percentage = dataValues.map(x => Math.round(x * 1000 / sum / 10))
 
-  const getData = i => {
-    return {
-      name: dataNames[i],
-      value: dataValues[i],
-      percent: percentage[i]
+  const getBasicDataForArr = x => {
+    const getDataArr = i => {
+      return {
+        name: dataNames[i],
+        value: dataValues[i],
+        percent: percentage[i]
+      }
     }
-  }
-
-  const getBasicValuesForArr = x => {
     for (let i in dataNames) {
-      x.push(getData(i))
+      x.push(getDataArr(i))
     }
   }
+
+  let dataArr = []
+  getBasicDataForArr(dataArr)
+  dataArr.sort((a, b) => a.value - b.value).reverse()
   const addColorToArr = arr => arr.map((x, i) => (x.color = colors[i]))
+  addColorToArr(dataArr)
 
-  const dataForDrawChart = []
-  getBasicValuesForArr(dataForDrawChart)
-  dataForDrawChart.sort((a, b) => a.value - b.value).reverse()
-  addColorToArr(dataForDrawChart)
+  return dataArr
+}
 
-  // Creating SVG chart
+// creating chart function
+
+const createPieChart = () => {
+  const chartContainer = document.querySelector('.article--chart')
+  chartContainer.style.display = "flex"
+
+  const titleContainer = document.querySelector('.title--chart')
+  const chartTitle = document.querySelector('#chartTitle').value
+  titleContainer.textContent = chartTitle
+
+  const dataArr = getData()
+
+  // creating SVG chart
 
   const radius = svg.style.width.slice(0, -2) / 2
+  let rotation = 0
 
   const calculateDeg = percent => 360 * percent / 100
   const calculateBaseDeg = deg => deg > 180 ? 360 - deg : deg
   const calculateRadian = baseDeg => baseDeg * Math.PI / 180
 
-  const calculateRatios = (percent) => {
+  const calculateRatios = percent => {
     const deg = calculateDeg(percent)
     const baseDeg = calculateBaseDeg(deg)
     const radians = calculateRadian(baseDeg)
@@ -141,22 +148,30 @@ const drawPieChart = () => {
     return [deg, sweep, valueX, valueY]
   }
 
-  const drawSegment = (data, i) => {
+  const drawSlice = (data, i) => {
     const [deg, sweep, valueX, valueY] = calculateRatios(data.percent)
+
+    if (i === 0) {
+      const svg = document.querySelector('.svg')
+      svg.style.background = dataArr[dataArr.length - 1].color
+    }
 
     const newPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
     newPath.setAttributeNS(null, 'fill', data.color)
     newPath.setAttributeNS(null, 'd', `M${ radius },${ radius } L${ radius },0 A${ radius },${ radius } ${ sweep } 1,1 ${ valueX }, ${ valueY } z`)
     newPath.textContent = `Name: ${ data.name }, Value: ${ data.value }, Percent: ${ data.percent }%`
-    newPath.setAttributeNS(null, 'transform', `rotate(${ rotation }, ${ radius }, ${ radius })`)
+    if (i !== dataArr.length - 1) {
+      newPath.setAttributeNS(null, 'transform', `rotate(${ rotation }, ${ radius }, ${ radius })`)
+    } else {
+      newPath.setAttributeNS(null, 'transform', `rotate(-${ deg }, ${ radius }, ${ radius })`)
+    }
 
     rotation += deg
     svg.appendChild(newPath)
   }
 
-  let rotation = 0
-  for (let i = 0; i < numberOfValues; i++) {
-    drawSegment(dataForDrawChart[i], i)
+  for (let i = 0; i < dataArr.length; i++) {
+    drawSlice(dataArr[i], i)
   }
 }
 
@@ -167,9 +182,7 @@ const buttonCreateChart = document.querySelector('.button--createChart')
 buttonCreateChart.addEventListener('click', () => {
   const dataValues = getValues('sliceValue')
 
-  if (dataValues.every(x => !isNaN(x))) {
-    drawPieChart()
-  }
+  dataValues.every(x => !isNaN(x)) && createPieChart()
 })
 
 // Tooltip
